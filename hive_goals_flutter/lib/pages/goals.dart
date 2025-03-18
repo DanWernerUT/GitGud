b23d30ec1eee20a9b63fe6343a38cc22ca764da0
Daniel Werner <d00493805@utahtech.edu>
2025-03-17 22:25:51 -0600

    added empty goals page

---
+++ hive_goals_flutter/lib/pages/goals.dart
@@ -0,0 +1,5 @@
+import 'package:hive_goals_flutter/models/goal_model.dart';
+import 'package:hive_goals_flutter/models/user_model.dart';
+import 'package:hive_goals_flutter/services/database_helper.dart';
+import 'package:hive_goals_flutter/res/hive_colors.dart';
+
